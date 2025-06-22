b4d840f9ae8c349b7c7b0b09f1812d56625e4bd2
victor <gfeng005@uottawa.ca>
2025-06-22 16:09:53 -0400

    created a class of ISPSubscriber

---
+++ design-principles-open-closed-principle/src/main/java/org/example/ISPSubscriber.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ISPSubscriber {
+    private Long subscriberId;
+    private String address;
+    private Long phoneNumber;
+    private int baseRate;
+    private long freeUsage;
+
+    public double calculateBill() {
+        List<InternetSessionHistory.InternetSession> sessions = InternetSessionHistory.getCurrentSessions(subscriberId);
+        long totalData = sessions.stream().mapToLong(InternetSessionHistory.InternetSession::getDataUsed).sum();
+        long chargeableData = totalData - freeUsage;
+        return (double) (chargeableData * baseRate) /100;
+    }
+}
