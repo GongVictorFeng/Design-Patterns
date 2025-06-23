b33c59e7b9603dffbda50406184f2ccd74176692
victor <gfeng005@uottawa.ca>
2025-06-23 15:06:15 -0400

    created an interface of PersistenceService

---
+++ design-principles-interface-segregation-principle/src/main/java/org/example/service/PersistenceService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import org.example.entity.Entity;
+
+import java.util.List;
+
+public interface PersistenceService<T extends Entity> {
+    void save(T entity);
+    void delete(T entity);
+    T findById(Long id);
+    public List<T> findByName(String name);
+}
