af8e0ae34261a5d22a0f6aa662140d6635061eca
victor <gfeng005@uottawa.ca>
2025-06-23 16:30:44 -0400

    used dependency injects to make the method - writeMessage depends on abstract and only contains business rule

--- design-principles-dependency-inversion/src/main/java/org/example/App.java
+++ design-principles-dependency-inversion/src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Hello world!
@@ -12,6 +13,8 @@ public class App
     {
         Message msg = new Message("This is a message again");
         MessagePrinter printer = new MessagePrinter();
-        printer.writeMessage(msg, "test_msg.txt");
+        try(PrintWriter writer = new PrintWriter("test_msg.txt")) {
+            printer.writeMessage(msg, new JSONFormatter(), writer);
+        }
     }
 }
