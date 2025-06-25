9d649b3bb9ae0f80bdbffcc0b2679decfd87c3d6
victor <gfeng005@uottawa.ca>
2025-06-25 13:41:13 -0400

    created Address and User class

---
+++ builder/src/main/java/org/example/Address.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Address {
+    private String houseNumber;
+
+    private String street;
+
+    private String city;
+
+    private String zipcode;
+
+    private String state;
+}
