5879ac729e7803cac24a8b010a6fdc5f82959635
victor <gfeng005@uottawa.ca>
2025-06-27 21:49:53 -0400

    created two concrete creators which implement abstract creator

---
+++ factory-method/src/main/java/org/example/TextMessageCreator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.message.Message;
+import org.example.message.TextMessage;
+
+public class TextMessageCreator extends MessageCreator {
+    @Override
+    public Message createMessage() {
+        return new TextMessage();
+    }
+}
