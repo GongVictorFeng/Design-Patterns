48c6d669a7c650dad9743eca294326bcc738c541
victor <gfeng005@uottawa.ca>
2025-06-27 21:49:20 -0400

    created the abstract creator

---
+++ factory-method/src/main/java/org/example/MessageCreator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.message.Message;
+
+/**
+ * This is our abstract "creator".
+ * The abstract method createMessage() has to be implemented by
+ * its subclasses.
+ */
+public abstract class MessageCreator {
+    public Message getMessage() {
+        Message msg = createMessage();
+        msg.addDefaultHeaders();
+        msg.encrypt();
+        return msg;
+    }
+
+    public abstract Message createMessage();
+}
