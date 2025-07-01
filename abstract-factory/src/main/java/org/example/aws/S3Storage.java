313a870c7d6bdcd423138838fecf0d2dcfe5756c
victor <gfeng005@uottawa.ca>
2025-06-30 23:21:01 -0400

    created concrete S3 storage and GoogleClo

---
+++ abstract-factory/src/main/java/org/example/aws/S3Storage.java
@@ -0,0 +1,21 @@
+package org.example.aws;
+
+import org.example.Storage;
+
+//Represents a concrete product in a family "Amazon Web Service"
+public class S3Storage implements Storage {
+
+    public S3Storage(int capacityInMib) {
+        //Use aws s3 api
+        System.out.println("Allocated "+capacityInMib+" on S3");
+    }
+    @Override
+    public String getId() {
+        return "S31";
+    }
+
+    @Override
+    public String toString() {
+        return "S3 Storage";
+    }
+}
