35d8592c355d89493fdf8379e85062962a7e7849
Victor Feng <gfeng005@uottawa.ca>
2025-07-13 04:23:51 +0800

    created client to use abstract factory design pattern

--- abstract-factory/src/main/java/org/example/App.java
+++ abstract-factory/src/main/java/org/example/App.java
@@ -1,13 +1,31 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.example.aws.AWSResourceFactory;
+import org.example.gcp.GoogleResourceFactory;
+
 public class App 
 {
+    private final ResourceFactory resourceFactory;
+    public App(ResourceFactory resourceFactory) {
+        this.resourceFactory = resourceFactory;
+    }
+    public Instance createServer(Instance.Capacity capacity, int storageMib) {
+        Instance instance = resourceFactory.createInstance(capacity);
+        Storage storage = resourceFactory.createStorage(storageMib);
+        instance.attachStorage(storage);
+        return instance;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App aws = new App(new AWSResourceFactory());
+        Instance i1 = aws.createServer(Instance.Capacity.micro, 20480);
+        i1.start();
+        i1.stop();
+
+        System.out.println("****************************************************");
+        App gcp = new App(new GoogleResourceFactory());
+        i1 = gcp.createServer(Instance.Capacity.micro, 20480);
+        i1.start();
+        i1.stop();
     }
 }
