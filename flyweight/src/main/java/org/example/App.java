c07d908fc2b377081f9d4d3721c5b4d28cf1a701
Victor Feng <gfeng005@uottawa.ca>
2025-07-28 14:27:32 +0800

    created client and test cases

--- flyweight/src/main/java/org/example/App.java
+++ flyweight/src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SystemErrorMessage msg1 = ErrorMessageFactory.getInstance().getError(ErrorMessageFactory.ErrorType.GenericSystemError);
+        System.out.println(msg1.getText("9527"));
+
+        System.out.println("****************************************************************");
+
+        UserBannedErrorMessage msg2 = ErrorMessageFactory.getInstance().getUserBannedMessage("1202");
+        System.out.println(msg2.getText(null));
     }
 }
