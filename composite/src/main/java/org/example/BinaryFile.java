55273b6c1d74bce0406d3967f4a2100a617c88f7
Victor Feng <gfeng005@uottawa.ca>
2025-08-11 17:56:25 +0800

    created the leaf node in composite pattern

---
+++ composite/src/main/java/org/example/BinaryFile.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class BinaryFile extends File {
+
+    private final long size;
+
+    public BinaryFile(String name, long size) {
+        super(name);
+        this.size = size;
+    }
+
+    @Override
+    public void ls() {
+        System.out.println(getName() + "\tsize " + size);
+    }
+
+    @Override
+    public void addFile(File file) {
+        throw new UnsupportedOperationException("Leaf node does not support add operation");
+    }
+
+    @Override
+    public File[] getFiles() {
+        throw new UnsupportedOperationException("Leaf node does not support get operation");
+    }
+
+    @Override
+    public boolean removeFile(File file) {
+        throw new UnsupportedOperationException("Leaf node does not support remove operation");
+    }
+}
