bc8c6ff70ab8914041f51d3ad396b04dcdb5e8a5
victor <gfeng005@uottawa.ca>
2025-10-14 21:41:01 -0400

    created the email facade

---
+++ facade/src/main/java/org/example/email/EmailFacade.java
@@ -0,0 +1,17 @@
+package org.example.email;
+
+import org.example.Order;
+
+public class EmailFacade {
+    public boolean sendOrderEmail(Order order) {
+        Template template = TemplateFactory.createTemplateFor(Template.TemplateType.Email);
+        Stationary stationary = StationaryFactory.createStationary();
+        Email email = Email.getBuilder()
+                .withTemplate(template)
+                .withStationary(stationary)
+                .forObject(order)
+                .build();
+        Mailer mailer = Mailer.getMailer();
+        return mailer.send(email);
+    }
+}
