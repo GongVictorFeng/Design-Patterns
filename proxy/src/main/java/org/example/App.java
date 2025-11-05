8856f7ab1493c4a269abb34076652d81c587b930
victor <gfeng005@uottawa.ca>
2025-11-05 13:23:32 -0500

    updated the client and test case

--- proxy/src/main/java/org/example/App.java
+++ proxy/src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import javafx.geometry.Point2D;
+import org.example.dynamic.DynamicImageFactory;
+
 public class App
 {
     public static void main( String[] args )
@@ -10,5 +12,10 @@ public class App
         System.out.println("Image location :" + img.getLocation());
         System.out.println("Rendering image now......");
         img.render();
+        System.out.println("****************************************");
+        Image dImg = DynamicImageFactory.getImage("B1.bmp");
+        dImg.setLocation(new Point2D(-10,0));
+        System.out.println(dImg.getLocation());
+        dImg.render();
     }
 }
