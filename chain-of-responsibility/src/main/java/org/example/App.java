a58af0d028ee3466ab831062879d43ebcaa33d7d
victor <gfeng005@uottawa.ca>
2025-11-09 21:25:43 -0500

    created the client and test cases

--- chain-of-responsibility/src/main/java/org/example/App.java
+++ chain-of-responsibility/src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.example.LeaveApplication.Type;
+
+import java.time.LocalDate;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LeaveApplication application = LeaveApplication.getBuilder().withType(Type.Sick)
+                                        .from(LocalDate.now()).to(LocalDate.now().plusDays(10))
+                                        .build();
+        System.out.println(application);
+        System.out.println("**************************************************");
+        LeaveApprover approver = createChain();
+        approver.processLeaveApplication(application);
+        System.out.println(application);
+    }
+
+    private static LeaveApprover createChain() {
+        Director director = new Director(null);
+        Manager manager = new Manager(director);
+        return new ProjectLead(manager);
     }
 }
